a095fb602e2344eb1a0fe2e9b09bc6ba524eb65c
Steven Levis <levis501@yahoo.com>
2021-07-22 21:14:21 -0400

    Added TouchOrMouse to simulate touches with mouseButton0

---
+++ Runtime/TouchOrMouse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+public class TouchOrMouse
+{
+
+  public static int touchCount
+  {
+    get
+    {
+      if (Input.touchCount > 0) { return Input.touchCount; }
+      if (Input.GetMouseButton(0) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
+      {
+        return 1;
+      }
+      return 0;
+    }
+  }
+
+  private static Touch _mouseTouch;
+  public static Touch mouseTouch
+  {
+    get { return _mouseTouch; }
+  }
+
+  public static Touch GetTouch(int i)
+  {
+    if (Input.touchCount > 0) { return Input.GetTouch(i); }
+    _mouseTouch.phase = TouchPhase.Canceled;
+    if (Input.GetMouseButtonDown(0)) { _mouseTouch.phase = TouchPhase.Began; }
+    else if (Input.GetMouseButtonUp(0)) { _mouseTouch.phase = TouchPhase.Ended; }
+    else if (Input.GetMouseButton(0)) {
+      if (_mouseTouch.position == Input.mousePosition.XY())
+      {
+        _mouseTouch.phase = TouchPhase.Stationary;
+      } else
+      {
+        _mouseTouch.phase = TouchPhase.Moved;
+      }
+    } else
+    {
+      _mouseTouch.phase = TouchPhase.Canceled;
+    }
+    return mouseTouch;
+  }
+}
